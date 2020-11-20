9e925bb84d50e6d7662af3959b93f5668eb1b746
L1gh73r <71175906+L1gh73r@users.noreply.github.com>
2020-11-20 19:06:36 +0800

    Update guess2.cpp

--- guess2.cpp
+++ guess2.cpp
@@ -15,30 +15,24 @@ using namespace std;
 
 list<string> numList;
 string RanddomNum();
-int countnumbers(const string& guess, const string& str);
-int countpositions(const string& guess, const string& str);
-string GetNextGuess();
-int64_t getCurrentTime()      //直接调用这个函数就行了，返回值最好是int64_t，long long应该也可以
-    {    
-       struct timeval tv;    
-       gettimeofday(&tv,NULL);    //该函数在sys/time.h头文件中
-       return tv.tv_sec * 1000 + tv.tv_usec / 1000;    
-    }    
+int64_t getCurrentTime();
+int countnumbers(const string& thenumber, const string& str);
+int countpositions(const string& thenumber, const string& str);
 int main()
 {
     int x = 1;
     int n, p;    // 分别表示有几个数字猜对，有几个数字位置也对
-    char buf[5] = { 0 };
+    char alist[5] = { 0 };
     for (int i = '1'; i <= '9'; i++)
         for (int j = '0'; j <= '9'; j++)
             for (int k = '0'; k <= '9'; k++)
                 for (int l = '0'; l <= '9'; l++)
                 {
-                    buf[0] = i;
-                    buf[1] = j;
-                    buf[2] = k;
-                    buf[3] = l;
-                    numList.push_back(buf);
+                    alist[0] = i;
+                    alist[1] = j;
+                    alist[2] = k;
+                    alist[3] = l;
+                    numList.push_back(alist);
                 }   // 初始化
     //找到的文件的文件名
         string tmp;
@@ -57,41 +51,44 @@ int main()
 
     //   file.open("C:\\Users\\24657\\Desktop\\345.txt");
     //file << tmp << ":\n";
-    string guess = "1234";//RanddomNum(); // 第1回合随机猜数
+    string thenumber = "1234";//RanddomNum(); // 第1回合随机猜数
     while (1)
-    {   cout << "emmmmm...   " << guess << endl;
+    {   cout << "emmmmm...   " << thenumber << endl;
         //.open("C:\\Users\\24657\\Desktop\\result.txt")
 
 
-        file << guess << "\n";
+        file << thenumber << "\n";
 
         cin >> n >> p;
         if (n == 4 && p == 4)
         {
-            cout << guess << "    goodgame!";// 猜中
+            cout << thenumber << "    goodgame!";// 猜中,直接goodgame，循环结束。
             break;
         }
         for (list<string>::iterator it = numList.begin();
-            it != numList.end();)
+            it != numList.end();)//遍历链表里面所有的四位数
         {
-            if (countnumbers(guess, *it) != n) // 如果没有n个数字对
+            if (countnumbers(thenumber, *it) != n) // 这个四位数中没有猜对的数字
             {
-                it = numList.erase(it);  // 不可能是被猜数，筛掉
+                it = numList.erase(it);  // 不可能是被猜数，删除
                 continue;
             }
-            if (countpositions(guess, *it) != p) // 如果没有p个数字位置对
+            if (countpositions(thenumber, *it) != p) // 这个四位数中没有猜对位置的数字
             {
-                it = numList.erase(it);  // 不可能是被猜数，筛掉
+                it = numList.erase(it);  // 不可能是被猜数，删除
                 continue;
             }
             ++it;
         }
-        if (numList.empty()) // 没有候选数了，人欺骗计算机
+        if (numList.empty()) // 没有候选数了，人输入的指令有问题
         {
             cout << "wrong" << endl;
             break;
         }
-        guess = GetNextGuess();
+        for (list<string>::iterator it = numList.begin();it != numList.end(); ++it)//将猜的数转换为链表中最大的数字
+        {
+            thenumber=*it;
+        }
     }
     file.close();
     return 0;
@@ -107,38 +104,34 @@ string RanddomNum()
     sprintf(buf, "%d", i);
     return buf;
 }
-int countnumbers(const string& guess, const string& num)
+int countnumbers(const string& thenumber, const string& num)
 {
     string s = num;  // 复制num，用于下面避免重复匹配的操作
     int count = 0;
-    for (int i = 0; i < 4; i++) // 对guess中每一个数字
+    for (int i = 0; i < 4; i++) // 对thenumber中每一个数字
     {
-        string::size_type pos = s.find(guess[i]);
+        string::size_type pos = s.find(thenumber[i]);
         if (pos != string::npos)
         {
             count++;
-            s[pos] = 'x';  // 为避免重复匹配，将它替换为字符’x’
+            s[pos] = 'o';  // 为避免重复匹配，将它替换为字符’o’
         }
     }
     return count;
 }
 
+int64_t getCurrentTime()      //直接调用这个函数就行了，返回值最好是int64_t，long long应该也可以
+    {    
+       struct timeval tv;    
+       gettimeofday(&tv,NULL);    //该函数在sys/time.h头文件中
+       return tv.tv_sec * 1000 + tv.tv_usec / 1000;    
+    }    
 
-int countpositions(const string& guess, const string& str)
+int countpositions(const string& thenumber, const string& str)
 {
     int count = 0;
     for (int i = 0; i < 4; i++)
-        count += (guess[i] == str[i]);
+        count += (thenumber[i] == str[i]);
     return count;
 }
 
-string GetNextGuess()
-{
-    string guess;   // 猜这个数
-    for (list<string>::iterator it = numList.begin();
-        it != numList.end(); ++it)
-    {
-        guess=*it;
-    }
-    return guess;
-}
